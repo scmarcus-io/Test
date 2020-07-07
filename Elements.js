fb06e82394d0650e033ab4a199181774038708a7
Samantha Marcus <samanthamarcus@Samanthas-Air-3.localdomain>
2020-07-06 22:43:12 -0400

    Updated pages

---
+++ Elements.js
@@ -0,0 +1,11 @@
+JS
+
+
+/**
+ * Generate a flexbox for every set of tiles
+ */
+}
+function generateAllTiles(prefix, tilesJSON) {
+  for (x in TILES) {
+    generateTiles(prefix, tilesJSON[x], x);
+}
